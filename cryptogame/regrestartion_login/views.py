09be74605285919af85cc8a9533299a879bd7d36
om badhe <81210497+frozbite079@users.noreply.github.com>
2024-07-15 10:11:56 +0000

    new

--- cryptogame/regrestartion_login/views.py
+++ cryptogame/regrestartion_login/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def login(request):
     
 
-    return render(request,'E:/crypto_project/static/html/login.html')
+    return render(request,'/workspaces/crypto_project/static/html/login.html')
